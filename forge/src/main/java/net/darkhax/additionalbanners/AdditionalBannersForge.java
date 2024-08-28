07a9af5f3e422c8efc6a9cd5f8f8f6261821eeec
lev <aw@a0.ax>
2024-08-28 15:25:38 -0500

    Fork AdditionalBanners

---
+++ forge/src/main/java/net/darkhax/additionalbanners/AdditionalBannersForge.java
@@ -0,0 +1,12 @@
+package net.darkhax.additionalbanners;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class AdditionalBannersForge {
+
+    public AdditionalBannersForge() {
+
+        Content.init();
+    }
+}
